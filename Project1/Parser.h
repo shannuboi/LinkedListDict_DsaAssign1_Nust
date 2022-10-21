06e48fc8ebad6926ea34f13111c8d0ead7b6bb57
shannu boi <shayan.khan.xyz@gmail.com>
2022-10-22 01:09:03 +0500

    Added PrintReverse function. Q3 done.

--- Project1/Parser.h
+++ Project1/Parser.h
@@ -6,15 +6,36 @@ using namespace std;
 
 string filename; //stores filename
 
+
+// FUNCTIONS NEEDED FOR ASSIGNMENT DEFINED IN THIS SUB-CLASS
 class DictList : public SinglyLinkedList<Dictionary>
 {
 public:
-	void WriteToDictionaryFile(fstream& file) const override
+	void WriteToDictionaryFile(fstream& file) const
 	{
 		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
 		{
 			file << cur->data.word << " " << cur->data.meaning << "\n";
 		}
+	}
+	void Print() const
+	{
+		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
+		{
+			cout << cur->data << "\n";
+		}
+	}
+	void PrintReverse() const
+	{
+		PrintReverseRecurrsive(start);
+	}
+
+private:
+	void PrintReverseRecurrsive(ListNode* node) const
+	{
+		// if next is not null then recurse
+		if (node->next) PrintReverseRecurrsive(node->next);
+		cout << node->data << "\n";
 	}
 };
 
@@ -23,11 +44,11 @@ void readFileData(DictList& list) //reads text from a file and writes it to the 
 	Dictionary dict;
 	string word, meaning;
 	fstream fin;
-	cout << "\e[46mEnter the filename\x1b[0m "; 
+	cout << "\x1b[46mEnter the filename\x1b[0m "; 
 	cin.ignore();
 	cin >> filename;
 	fin.open(filename);
-	cout << "\e[0;32mFile reading succesful.\x1b[0m\n";
+	cout << "\x1b[0;32mFile reading succesful.\x1b[0m\n";
 	while( fin >> word )  //write into file name
 	{
 		fin.ignore();
@@ -42,7 +63,7 @@ void readFileData(DictList& list) //reads text from a file and writes it to the 
 void writeToFile(DictList& list) //write the linked list to a text file
 {
 	fstream dictFile; //create fstream object for the file
-	cout << "\e[46mEnter the filename\x1b[0m "; 
+	cout << "\x1b[46mEnter the filename\x1b[0m "; 
 	cin.ignore();
 	cin >> filename;
 	dictFile.open(filename, std::ios::app); //create/open a text file in append mode. new information is always added to the end
@@ -50,39 +71,54 @@ void writeToFile(DictList& list) //write the linked list to a text file
 	list.WriteToDictionaryFile(dictFile);
 
 	dictFile.close();
-	cout << "\e[0;32mDictionary entries added.\x1b[0m\n";
+	cout << "\x1b[0;32mDictionary entries added.\x1b[0m\n";
 }
 
 void mainMenu() //menu function
 {
-  string word, meaning; //stores the word and meaning
-  char choice = 0;  //stores user choice for the actions
-  DictList list;
+	string word, meaning; //stores the word and meaning
+	char choice = 0;  //stores user choice for the actions
+	DictList list;
 
-  cout << "\e[1;35mInput 's' to terminate the program anytime.\e[0;37m" << endl;
-  while (choice != 's') //while loop until 's' is entered
-  {
-    cout << "\n\x1b[36mPlease select an action: " << endl << endl;
-    cout << "\t1. Read file data" << endl;
-    cout << "\t2. Write to file" << endl;
+	cout << "\x1b[1;35mInput 's' to terminate the program anytime.\x1b[0;37m" << endl;
+	while (choice != 's') //while loop until 's' is entered
+	{
+		cout << "\n\x1b[36mPlease select an action: " << endl << endl;
+		cout << "\t1. Read file data to list" << endl;
+		cout << "\t2. Write to file from lisy" << endl;
+		cout << "\t3. Print list" << endl;
+		cout << "\t4. Print Reversed list" << endl;
 
-    cout << "\x1b[0m"; cin >> choice; cout << endl;
-    switch(choice)  //switch statement to check user input and calling respective functions
-    {
-      case('1'):
-      {
-        readFileData(list);
-      }; break;
-      case('2'):
-      {
-        writeToFile(list);
-      }; break;
-      case('s'):
-      {
-        cout << "\e[1;33mProgram terminated." << endl;
-      }; break;
-      default:
-        cout << "\x1b[31mInvalid action selected!\x1b[37m" << endl;
-    }
-  }
+		cout << "\x1b[0m"; cin >> choice; cout << endl;
+		switch(choice)  //switch statement to check user input and calling respective functions
+		{
+		case('1'):
+		{
+			readFileData(list);
+		}; break;
+
+		case('2'):
+		{
+			writeToFile(list);
+		}; break;
+
+		case('3'):
+		{
+			list.Print();
+		}; break;
+
+		case('4'):
+		{
+			list.PrintReverse();
+		}; break;
+
+		case('s'):
+		{
+			cout << "\x1b[1;33mProgram terminated." << endl;
+		}; break;
+
+		default:
+			cout << "\x1b[31mInvalid action selected!\x1b[37m" << endl;
+		}
+	}
 }
