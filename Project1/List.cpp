05ba44470551a200246a74b31b6818b7a8e860b3
shannu boi <shayan.khan.xyz@gmail.com>
2022-10-21 19:22:31 +0500

    More functionality added to list class

--- Project1/List.cpp
+++ Project1/List.cpp
@@ -1,5 +1,30 @@
 #include "List.h"
 
+SinglyLinkedList::~SinglyLinkedList()
+{
+	ClearList();
+}
+
+SinglyLinkedList::SinglyLinkedList(const SinglyLinkedList & src)
+{
+	*this = src;
+}
+
+void SinglyLinkedList::operator=(const SinglyLinkedList & rhs)
+{
+	if (this != &rhs)
+	{
+		ClearList();
+
+		const ListNode* rhsptr = rhs.start;
+		for (int i = 0; rhsptr; i++) // loop until rhsptr becomes null
+		{
+			Insert(rhsptr->data, i);
+			rhsptr = rhsptr->next;
+		}
+	}
+}
+
 bool SinglyLinkedList::IsEmpty() const
 {
 	return !start; // if start hold nothing return true
@@ -7,17 +32,77 @@ bool SinglyLinkedList::IsEmpty() const
 
 bool SinglyLinkedList::Search(Dictionary d) const
 {
-	for (ListNode* cur = start; cur; cur = cur->next)
+	for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
 	{
 		if (cur->data == d) return true;
 	}
 	return false;
 }
 
-void SinglyLinkedList::TopInsert(Dictionary d)
+// First pos is zero
+void SinglyLinkedList::Insert(Dictionary d, int pos)
 {
-	ListNode* newnode = new ListNode;
-	newnode->data = d;
-	newnode->next = start;
-	start = newnode;
+	if (pos == 0)
+	{
+		ListNode* newnode = new ListNode;
+		newnode->data = d;
+		newnode->next = start;
+		start = newnode;
+	}
+	else
+	{
+		InsertAfter(d, start, pos-1); // minus one sice insert after
+	}
+}
+
+void SinglyLinkedList::Delete(Dictionary d)
+{
+	ListNode* cur = start;
+	ListNode* last;
+	while (cur || cur->data != d) // loop until cur becomes null
+	{
+		last = cur;
+		cur = cur->next;
+	}
+
+	if (cur) // if item found
+	{
+		if (last) //if it not start
+		{
+			last->next = cur->next;
+			delete cur;
+		}
+		else // if it is start
+		{
+			start = cur->next;
+			delete cur;
+		}
+	}
+}
+
+void SinglyLinkedList::ClearList()
+{
+	for (ListNode* cur = start; cur;) // loop until cur becomes null
+	{
+		ListNode* temp = cur;
+		cur = cur->next;
+		delete temp;
+	}
+	start = nullptr;
+}
+
+// Since it will insert after a node, pos acts like pos + 1 (Insertion happens after specified pos)  
+void SinglyLinkedList::InsertAfter(Dictionary d, ListNode * node, int pos)
+{
+	if (pos == 0)
+	{
+		ListNode* newnode = new ListNode;
+		newnode->data = d;
+		newnode->next = node->next;
+		node->next = newnode;
+	}
+	else
+	{
+		InsertAfter(d, node->next, pos - 1);
+	}
 }
--- Project1/List.h
+++ Project1/List.h
@@ -10,9 +10,15 @@ class SinglyLinkedList
 {
 public:
 	SinglyLinkedList() = default;
+	~SinglyLinkedList();
+	SinglyLinkedList(const SinglyLinkedList& src);
+	void operator=(const SinglyLinkedList& rhs);
 	bool IsEmpty() const;
 	bool Search(Dictionary d) const;
-	void TopInsert(Dictionary d);
+	void Insert(Dictionary d, int pos = 0);
+	void Delete(Dictionary d);
+	void ClearList();
 private:
+	void InsertAfter(Dictionary d, ListNode* node, int pos);
 	ListNode* start = nullptr;
 };
