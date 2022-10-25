3b0a288a628e2b73cabaa1a3626c9aa971f00059
shannu boi <shayan.khan.xyz@gmail.com>
2022-10-25 14:12:37 +0500

    Assignment Changed. New Parser.h file

--- Project1/Parser.h
+++ Project1/Parser.h
@@ -6,198 +6,74 @@ using namespace std;
 
 string filename; //stores filename
 
-
-// FUNCTIONS NEEDED FOR ASSIGNMENT DEFINED IN THIS SUB-CLASS
-class DictList : public SinglyLinkedList<Dictionary>
+void readFileData(ListNode **headNode) //reads text from a file. You should add functionality to store the fetched data into a singly linked list. 
 {
-public:
-	void WriteToDictionaryFile(fstream& file) const
-	{
-		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
-		{
-			file << cur->data.word << " " << cur->data.meaning << "\n";
-		}
-	}
-	void Print() const
-	{
-		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
-		{
-			cout << cur->data << "\n";
-		}
-	}
-	void PrintReverse() const
-	{
-		PrintReverseRecurrsive(start);
-	}
-	void ReverseList()
-	{
-		ListNode* prev = nullptr;
-		ListNode* cur = start;
-		
-		while (cur) // loop until cur becomes null
-		{
-			ListNode* tempNext = cur->next;
-
-			cur->next = prev;
-			prev = cur;
-			cur = tempNext;
-		}
-		start = prev;
-	}
-	void FilterByLetter(string letter)
-	{
-		for (ListNode* cur = start; cur;) // loop until cur becomes null
-		{
-			if (cur->data.word.find(letter) == string::npos) // if it dosn't contain the letter
-			{
-				Dictionary toBeDel = cur->data;
-				cur = cur->next;
-				Delete(toBeDel);
-			}
-			else
-			{
-				cur = cur->next;
-			}
-		}
-	}
-	void RearrangeByEvenOddLength()
-	{
-		DictList tempList;
-		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
-		{
-			if (cur->data.word.size() % 2 == 1)
-			{
-				tempList.Insert(cur->data);
-			}
-		}
-
-		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
-		{
-			if (cur->data.word.size() % 2 == 0)
-			{
-				tempList.Insert(cur->data);
-			}
-		}
-
-		*this = tempList;
-	}
-
-private:
-	void PrintReverseRecurrsive(ListNode* node) const
-	{
-		// if next is not null then recurse
-		if (node->next) PrintReverseRecurrsive(node->next);
-		cout << node->data << "\n";
-	}
-};
-
-void readFileData(DictList& list) //reads text from a file and writes it to the AVL tree
-{
-	Dictionary dict;
-	string word, meaning;
-	fstream fin;
-	cout << "\x1b[46mEnter the filename\x1b[0m "; 
-	cin.ignore();
-	cin >> filename;
-	fin.open(filename);
-	cout << "\x1b[0;32mFile reading succesful.\x1b[0m\n";
-	while( fin >> word )  //write into file name
-	{
-		fin.ignore();
-		getline(fin,meaning);
-		dict = Dictionary(word,meaning);
-	
-		list.Insert(dict);
-	}
-	list.ReverseList();
-
-	fin.close();
+  Dictionary *dict;
+  string word, meaning;
+  fstream fin;
+  cout << "\e[46mEnter the filename\x1b[0m "; 
+  cin.ignore();
+  cin >> filename;
+  fin.open(filename);
+  cout << "\e[0;32mFile reading succesful.\x1b[0m\n";
+  while( fin >> word )  //write into file name
+  {
+    fin.ignore();
+    getline(fin,meaning);
+    dict = new Dictionary(word,meaning);
+    cout << *dict << endl; //output newly created Dictionary object
+    //remove above line and insert the new node to the linked list here
+  }
+  fin.close();
 }
 
-void writeToFile(DictList& list) //write the linked list to a text file
+void writeToFile(ListNode *headNode) //write the linked list to a text file
 {
-	fstream dictFile; //create fstream object for the file
-	cout << "\x1b[46mEnter the filename\x1b[0m "; 
-	cin.ignore();
-	cin >> filename;
-	dictFile.open(filename, std::ios::app); //create/open a text file in append mode. new information is always added to the end
+  string word, meaning;
+  
+  fstream dictFile; //create fstream object for the file
+  cout << "\e[46mEnter the filename\x1b[0m "; 
+    cin.ignore();
+  cin >> filename;
+  dictFile.open(filename, std::ios::app); //create/open a text file in append mode. new information is always added to the end
 
-	list.WriteToDictionaryFile(dictFile);
-
-	dictFile.close();
-	cout << "\x1b[0;32mDictionary entries added.\x1b[0m\n";
+  //write to the textfile here
+  
+  dictFile.close();
+  cout << "\e[0;32mDictionary entries added.\x1b[0m\n";
 }
+
 
 void mainMenu() //menu function
 {
-	string word, meaning; //stores the word and meaning
-	char choice = 0;  //stores user choice for the actions
-	DictList list;
+  string word, meaning; //stores the word and meaning
+  char choice;  //stores user choice for the actions
 
-	cout << "\x1b[1;35mInput 's' to terminate the program anytime.\x1b[0;37m" << endl;
-	while (choice != 's') //while loop until 's' is entered
-	{
-		cout << "\n\x1b[36mPlease select an action: " << endl << endl;
-		cout << "\t1. Read file data to list" << endl;
-		cout << "\t2. Write to file from list" << endl;
-		cout << "\t3. Print list" << endl;
-		cout << "\t4. Print list in reverse" << endl;
-		cout << "\t5. Reverse list" << endl;
-		cout << "\t6. Filter list" << endl;
-		cout << "\t7. Rearrange by parity list" << endl;
+  cout << "\e[1;35mInput 's' to terminate the program anytime.\e[0;37m" << endl;
+  while (choice != 's') //while loop until 's' is entered
+  {
+    cout << "\n\x1b[36mPlease select an action: " << endl << endl;
+    cout << "\t1. Read file data" << endl;
+    cout << "\t2. Write to file" << endl;
+    cout << "\t3. Print List" << endl;
 
-		cout << "\x1b[0m"; cin >> choice; cout << endl;
-		switch(choice)  //switch statement to check user input and calling respective functions
-		{
-		case('1'):
-		{
-			readFileData(list);
-		}; break;
-
-		case('2'):
-		{
-			writeToFile(list);
-		}; break;
-
-		case('3'):
-		{
-			list.Print();
-		}; break;
-
-		case('4'):
-		{
-			list.PrintReverse();
-		}; break;
-
-		case('5'):
-		{
-			list.ReverseList();
-			cout << "\x1b[0;32mList Reversed.\x1b[0m\n";
-		}; break;
-
-		case('6'):
-		{
-			cout << "\x1b[46mEnter Filter Letter:\x1b[0m ";
-			string FilterLetter;
-			cin.ignore();
-			cin >> FilterLetter;
-			list.FilterByLetter(FilterLetter);
-			cout << "\x1b[0;32mList Filtered.\x1b[0m\n";
-		}; break;
-
-		case('7'):
-		{
-			list.RearrangeByEvenOddLength();
-			cout << "\x1b[0;32mList Rearranged.\x1b[0m\n";
-		}; break;
-
-		case('s'):
-		{
-			cout << "\x1b[1;33mProgram terminated." << endl;
-		}; break;
-
-		default:
-			cout << "\x1b[31mInvalid action selected!\x1b[37m" << endl;
-		}
-	}
+    cout << "\x1b[0m"; cin >> choice; cout << endl;
+    switch(choice)  //switch statement to check user input and calling respective functions
+    {
+      case('1'):
+      {
+        readFileData(&list.start);
+      }; break;
+      case('2'):
+      {
+        writeToFile(list.start);
+      }; break;
+      case('s'):
+      {
+        cout << "\e[1;33mProgram terminated." << endl;
+      }; break;
+      default:
+        cout << "\x1b[31mInvalid action selected!\x1b[37m" << endl;
+    }
+  }
 }
---
+++ Project1/ParserOLD.h
@@ -0,0 +1,203 @@
+#include<fstream>
+#include<iostream>
+#include "Dictionary.h"
+#include "List.h"
+using namespace std;
+
+string filename; //stores filename
+
+
+// FUNCTIONS NEEDED FOR ASSIGNMENT DEFINED IN THIS SUB-CLASS
+class DictList : public SinglyLinkedList<Dictionary>
+{
+public:
+	void WriteToDictionaryFile(fstream& file) const
+	{
+		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
+		{
+			file << cur->data.word << " " << cur->data.meaning << "\n";
+		}
+	}
+	void Print() const
+	{
+		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
+		{
+			cout << cur->data << "\n";
+		}
+	}
+	void PrintReverse() const
+	{
+		PrintReverseRecurrsive(start);
+	}
+	void ReverseList()
+	{
+		ListNode* prev = nullptr;
+		ListNode* cur = start;
+		
+		while (cur) // loop until cur becomes null
+		{
+			ListNode* tempNext = cur->next;
+
+			cur->next = prev;
+			prev = cur;
+			cur = tempNext;
+		}
+		start = prev;
+	}
+	void FilterByLetter(string letter)
+	{
+		for (ListNode* cur = start; cur;) // loop until cur becomes null
+		{
+			if (cur->data.word.find(letter) == string::npos) // if it dosn't contain the letter
+			{
+				Dictionary toBeDel = cur->data;
+				cur = cur->next;
+				Delete(toBeDel);
+			}
+			else
+			{
+				cur = cur->next;
+			}
+		}
+	}
+	void RearrangeByEvenOddLength()
+	{
+		DictList tempList;
+		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
+		{
+			if (cur->data.word.size() % 2 == 1)
+			{
+				tempList.Insert(cur->data);
+			}
+		}
+
+		for (ListNode* cur = start; cur; cur = cur->next) // loop until cur becomes null
+		{
+			if (cur->data.word.size() % 2 == 0)
+			{
+				tempList.Insert(cur->data);
+			}
+		}
+
+		*this = tempList;
+	}
+
+private:
+	void PrintReverseRecurrsive(ListNode* node) const
+	{
+		// if next is not null then recurse
+		if (node->next) PrintReverseRecurrsive(node->next);
+		cout << node->data << "\n";
+	}
+};
+
+void readFileData(DictList& list) //reads text from a file and writes it to the AVL tree
+{
+	Dictionary dict;
+	string word, meaning;
+	fstream fin;
+	cout << "\x1b[46mEnter the filename\x1b[0m "; 
+	cin.ignore();
+	cin >> filename;
+	fin.open(filename);
+	cout << "\x1b[0;32mFile reading succesful.\x1b[0m\n";
+	while( fin >> word )  //write into file name
+	{
+		fin.ignore();
+		getline(fin,meaning);
+		dict = Dictionary(word,meaning);
+	
+		list.Insert(dict);
+	}
+	list.ReverseList();
+
+	fin.close();
+}
+
+void writeToFile(DictList& list) //write the linked list to a text file
+{
+	fstream dictFile; //create fstream object for the file
+	cout << "\x1b[46mEnter the filename\x1b[0m "; 
+	cin.ignore();
+	cin >> filename;
+	dictFile.open(filename, std::ios::app); //create/open a text file in append mode. new information is always added to the end
+
+	list.WriteToDictionaryFile(dictFile);
+
+	dictFile.close();
+	cout << "\x1b[0;32mDictionary entries added.\x1b[0m\n";
+}
+
+void mainMenu() //menu function
+{
+	string word, meaning; //stores the word and meaning
+	char choice = 0;  //stores user choice for the actions
+	DictList list;
+
+	cout << "\x1b[1;35mInput 's' to terminate the program anytime.\x1b[0;37m" << endl;
+	while (choice != 's') //while loop until 's' is entered
+	{
+		cout << "\n\x1b[36mPlease select an action: " << endl << endl;
+		cout << "\t1. Read file data to list" << endl;
+		cout << "\t2. Write to file from list" << endl;
+		cout << "\t3. Print list" << endl;
+		cout << "\t4. Print list in reverse" << endl;
+		cout << "\t5. Reverse list" << endl;
+		cout << "\t6. Filter list" << endl;
+		cout << "\t7. Rearrange by parity list" << endl;
+
+		cout << "\x1b[0m"; cin >> choice; cout << endl;
+		switch(choice)  //switch statement to check user input and calling respective functions
+		{
+		case('1'):
+		{
+			readFileData(list);
+		}; break;
+
+		case('2'):
+		{
+			writeToFile(list);
+		}; break;
+
+		case('3'):
+		{
+			list.Print();
+		}; break;
+
+		case('4'):
+		{
+			list.PrintReverse();
+		}; break;
+
+		case('5'):
+		{
+			list.ReverseList();
+			cout << "\x1b[0;32mList Reversed.\x1b[0m\n";
+		}; break;
+
+		case('6'):
+		{
+			cout << "\x1b[46mEnter Filter Letter:\x1b[0m ";
+			string FilterLetter;
+			cin.ignore();
+			cin >> FilterLetter;
+			list.FilterByLetter(FilterLetter);
+			cout << "\x1b[0;32mList Filtered.\x1b[0m\n";
+		}; break;
+
+		case('7'):
+		{
+			list.RearrangeByEvenOddLength();
+			cout << "\x1b[0;32mList Rearranged.\x1b[0m\n";
+		}; break;
+
+		case('s'):
+		{
+			cout << "\x1b[1;33mProgram terminated." << endl;
+		}; break;
+
+		default:
+			cout << "\x1b[31mInvalid action selected!\x1b[37m" << endl;
+		}
+	}
+}
